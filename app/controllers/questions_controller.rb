548dd39a4a5f913af38fd4a9b6ccc6f087a0e2dd
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-11-04 15:24:19 +0900

    completed

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    case @question
+    when "I am going to work"
+      @answer = "Great!"
+    when /\?/
+      @answer = "Silly question, get dressed and go to work!"
+    when nil
+      @answer = ""
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
